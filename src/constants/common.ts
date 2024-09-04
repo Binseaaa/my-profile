15495e294fa19873e87770b303a30c0826f44188
Vincy Deguma <vinsmar8888@gmail.com>
2024-09-04 08:39:51 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Vince Marie Deguma';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Aspiring IT professional currently in my 4th year of studies, focusing on modern web development technologies. Skilled in writing, testing, and maintaining software code, with a growing expertise in programming languages, algorithms, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Passionate about developing web applications, I am gaining hands-on experience in both frontend and backend development. I work with tools like Laravel, Inertia.js, Vue.js, and JavaScript to create scalable and efficient solutions.';
+export const ABOUT_TEXT_3 =
+  'A versatile learner, I am building a strong foundation in full-stack development, with a focus on mastering frameworks and languages necessary for developing complete web applications. My goal is to bridge the gap between design and functionality to deliver user-friendly solutions.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Fourth-year IT student with a keen interest in full-stack web development. Currently learning and building projects using Laravel, Inertia.js, Vue.js, and modern JavaScript frameworks. Committed to expanding my knowledge in both front-end and back-end technologies.';
+export const RESUME_TEXT_2 =
+  'Student Developer with 3 years experience building websites and web applications.';
